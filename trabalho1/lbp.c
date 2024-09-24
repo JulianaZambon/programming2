48191ae7c1929bc42ca37721cf5256fa0062650a
Juliana Zambon <64793722+JulianaZambon@users.noreply.github.com>
2024-09-24 20:16:50 -0300

    Update lbp.c

--- trabalho1/lbp.c
+++ trabalho1/lbp.c
@@ -1,3 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    // Parsear argumentos de linha de comando (use getopt)
+    getopt(argc, argv, "i:r:o:");
+        
+    // Carregar ou calcular o LBP da imagem de teste
+
+    
+    // Para cada imagem na base de referência:
+    //     Verificar se o arquivo .lbp existe
+    //     Se não, calcular e salvar o vetor LBP
+    //     Calcular a distância Euclidiana entre os vetores LBP
+    
+    // Exibir a imagem com a menor distância
+    
+    return 0;
+}
