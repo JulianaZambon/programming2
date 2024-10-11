818a9180c6d6122699c62f41d34b8164cb3fdf6e
Juliana Zambon <64793722+JulianaZambon@users.noreply.github.com>
2024-10-11 20:11:18 -0300

    Update lib.h

--- trabalho1/lib.h
+++ trabalho1/lib.h
@@ -9,7 +9,7 @@ struct imagemPGM {
 };
 
 /* Função para calcular o LBP */
-void lbp(struct imagemPGM *img, int **lbp);
+void lbp(struct imagemPGM *img, unsigned char **lbp);
 
 /* Função para extrair um vetor de características que sumarize o conteúdo da imagem */
 void extrair_vetor_caracteristicas(struct imagemPGM *img, double *vetor);
@@ -21,18 +21,15 @@ double distancias_euclidianas(double *v1, double *v2, int n);
 void gerar_imagem_lbp(struct imagemPGM *img, int **lbp);
 
 /* Função para gravar o vetor LBP em um arquivo binário */
-void gravar_vetor_lbp(char *nome_arquivo, int **lbp, int largura, int altura);
+void gravar_vetor_lbp(const char *nome_arquivo, double *vetor_lbp, int tamanho);
 
 /* Função para carregar o vetor LBP de um arquivo binário */
-void carregar_vetor_lbp(char *nome_arquivo, double *vetor_lbp, int largura, int altura);
+void carregar_vetor_lbp(const char *nome_arquivo, double *vetor_lbp, int tamanho);
 
 /* Função para normalizar o histograma da imagem */
-/* Esta função será usada para normalizar o histograma da imagem
-dividindo os pixels pelo total, corrigindo assim diferenças de escala*/
 void normalizar_histograma(struct imagemPGM *img, double *histograma_normalizado);
 
 /* Função para ignorar comentários em arquivos PGM */
-/* Ignora linhas de comentários iniciadas com # nos arquivos PGM */
 void ignora_comentarios(FILE *arquivo);
 
 /* Função para ler uma imagem PGM (suporta P2 e P5) */
@@ -42,13 +39,13 @@ struct imagemPGM *ler_imagem(const char *nome_arquivo);
 void liberar_imagem(struct imagemPGM *img);
 
 /* Função para escrever uma imagem PGM (mantém o formato original: P2 ou P5) */
-/* As funções de leitura e escrita de imagens garantem que o formato da imagem
-de saída será o mesmo da imagem de entrada, seja P2 ou P5.*/
 void escrever_imagem(const char *nome_arquivo, struct imagemPGM *img, const char *tipo);
 
 /* Função para descartar bordas da imagem após o processo de convolução */
 struct imagemPGM *descartar_bordas(struct imagemPGM *img);
 
 #endif
+
+
 
 
