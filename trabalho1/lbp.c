dad78bb7ae6d0cf996065cc13d5f0794dd964420
Juliana Zambon <64793722+JulianaZambon@users.noreply.github.com>
2024-10-17 14:22:03 -0300

    Update lbp.c

--- trabalho1/lbp.c
+++ trabalho1/lbp.c
@@ -12,29 +12,28 @@
 
 int main(int argc, char *argv[])
 {
-    int opt;             /* opção de linha de comando */
-    // int gerar_saida = 0; /* flag para gerar a imagem de saída */
+    int opt; /* opção de linha de comando */
     char *arquivo_entrada = NULL;
-    // char *diretorio = NULL;
-    // char *arquivo_saida = NULL;
+    char *diretorio = NULL;
+    char *arquivo_saida = NULL;
     FILE *arquivo = NULL;
 
     while ((opt = getopt(argc, argv, "d:i:o:")) != -1)
     {
         switch (opt)
         {
-        // case 'd':
-        //     diretorio = strdup(optarg);
-        //     break;
+        case 'd':
+            diretorio = strdup(optarg);
+            break;
         case 'i':
             arquivo_entrada = optarg;
             break;
-        // case 'o':
-        //     arquivo_saida = optarg;
-        //     break;
-        // default:
-        // fprintf(stderr, "Uso: %s -d <diretório> -i <arquivo_entrada> -o <arquivo_saida>\n", argv[0]);
-        //     exit(1);
+        case 'o':
+            arquivo_saida = optarg;
+            break;
+        default:
+            fprintf(stderr, "Uso: %s -d <diretório> -i <arquivo_entrada> -o <arquivo_saida>\n", argv[0]);
+            exit(1);
         }
     }
 
@@ -66,58 +65,44 @@ int main(int argc, char *argv[])
     /* Inicializa a nova imagem */
     inicializar_nova_imagem(nova_img, img);
 
-    /* Gerar a imagem LBP */
-    /*
-     Exemplo:
-     ./lbp -i img1.tif -o img_out.tif
-     Nesse caso, o programa recebe como entrar imagem img1.tif e gerar a imagem LPB
-     com o nome informado pela opção -o. NENHUMA saída é esperada no terminal; caso
-     algum erro ocorra, basta não criar a imagem de saída.
-    */
-
     /* Gera a imagem LBP */
     gerar_lbp(img, nova_img);
-
     /* Abre o arquivo de saída */
-    FILE *arquivo_saida = fopen("lena_lbp.pgm", "wb");
-    /* wb = write binary */
-    gerar_imagem_saida(nova_img, arquivo_saida);
+    /* gerar a imagem LPB com o nome informado pela opção -o */
+    FILE *arquivo_saida_ptr = fopen("arquivo_lbp.pgm", "wb"); /* w = write, b = binary */
+    if (arquivo_saida_ptr == NULL)
+    {
+        fprintf(stderr, "Erro ao abrir o arquivo de saída\n");
+        free(arquivo_entrada);
+        fclose(arquivo);
+        exit(EXIT_FAILURE);
+    }
+    gerar_imagem_saida(nova_img, arquivo_saida_ptr);
 
-    /* Comparar uma imagem de teste com todas as imagens da base de referência */
-    /*  A saída deve ser EXATAMENTE a seguinte
-        Imagem mais similar: <img mais similar> <distância>
+    /*  Comparar uma imagem de teste com todas as imagens da base de referência
+        Exibir a imagem mais similar e a distância
+        printf("Imagem mais similar: %s %.6f\n", img_similar, distancia);
     */
 
-    /* Exibir a imagem mais similar e a distância */
-    /* printf("Imagem mais similar: %s %.6f\n", img_similar, distancia); */
-
-    /*
-        em que <img mais similar> é a imagem com a menor distância Euclidiana encontrada
-        no diretório ./base (informar apenas o nome do arquivo com sua extensão) e
-        <distância> é o valor da distância associada a esta imagem, um número com
-        EXATAMENTE 6 casas decimais. Nome e valor devem ser separados APENAS por
-        um espaço.
-    */
-
-    // struct LBPHistograma *histograma = alocar_histograma();
-    // gerar_histograma(nova_img, histograma, arquivo_entrada);
-    fclose(arquivo); /* fecha o arquivo de entrada */
+    struct LBPHistograma *histograma = alocar_histograma();
+    gerar_histograma(nova_img, histograma, arquivo_entrada);
+    fclose(arquivo);
 
     /* percorre o diretório */
-    // double distancia = 1e12;
-    // char menor_distancia[256];
+    double distancia = 1e12;
+    char menor_distancia[256];
+    ler_diretorio(diretorio);
 
-    /* ERRO A PARTIR DAQUI */
-    // ler_diretorio(diretorio);
-    //  encontrar_imagem_similar(diretorio, histograma, &distancia, menor_distancia);
+    /* Exibir a imagem mais similar e a distância */
+    encontrar_imagem_similar(diretorio, histograma, &distancia, menor_distancia);
 
-    // /* Libera a memória alocada */
-    // free (arquivo_entrada);
-    // free (arquivo_saida);
-    // free (diretorio);
-    // liberar_imagem(img);
-    // liberar_imagem(nova_img);
-    // free(histograma);
+    /* Libera a memória alocada */
+    free(arquivo_entrada);
+    free(arquivo_saida);
+    free(diretorio);
+    liberar_imagem(img);
+    liberar_imagem(nova_img);
+    free(histograma);
 
     return 0;
 }
