1fecd2327d5db782364a52364376e57c3903d041
Juliana Zambon <64793722+JulianaZambon@users.noreply.github.com>
2024-10-16 22:13:22 -0300

    Add files via upload

---
+++ trabalho1 (cópia)/lbp.c
@@ -0,0 +1,146 @@
+/* Autora: Juliana Zambon (jz22), GRR20224168 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <string.h>
+#include <stdint.h>
+#include <math.h>
+#include <dirent.h>
+#include "lib.h"
+
+int main(int argc, char *argv[])
+{
+    int opt;
+    char *arquivo_entrada = NULL;
+    char *diretorio = NULL;
+    char *arquivo_saida = NULL;
+    FILE *arquivo = NULL;
+    int aux = 0;
+
+    while ((opt = getopt(argc, argv, "d:i:o:")) != -1)
+    {
+        switch (opt)
+        {
+        // case 'd':
+        //     diretorio = strdup(optarg);
+        //     break;
+        case 'i':
+            arquivo_entrada = optarg;
+            break;
+        case 'o':
+            arquivo_saida = optarg;
+            break;
+        default:
+            fprintf(stderr, "Uso: %s -d <diretório> -i <arquivo_entrada> -o <arquivo_saida>\n", argv[0]);
+            exit(1);
+        }
+    }
+
+    // if (diretorio == NULL || arquivo_entrada == NULL || arquivo_saida == NULL)
+    // {
+    //     fprintf(stderr, "Uso: %s -d <diretório> -i <arquivo_entrada> -o <arquivo_saida>\n", argv[0]);
+    //     exit(EXIT_FAILURE);
+    // }
+
+    /* Lê o arquivo de entrada */
+    arquivo = fopen(arquivo_entrada, "r"); /* r = read */
+    if (arquivo == NULL)
+    {
+        fprintf(stderr, "Erro ao abrir o arquivo de entrada\n");
+        free(arquivo_entrada);
+        exit(1);
+    }
+
+    /* Aloca memória para a imagem */
+    struct imagemPGM *img = alocar_imagem();
+    // /* Lê a imagem */
+    img = ler_imagem(arquivo, img, arquivo_entrada);
+    if (img == NULL)
+    {
+        fprintf(stderr, "Erro ao ler a imagem\n");
+        free(arquivo_entrada);
+        fclose(arquivo);
+        exit(1);
+    }
+
+    /* Aloca memória para a nova imagem */
+    struct imagemPGM *nova_img = alocar_imagem();
+    /* Inicializa a nova imagem */
+    inicializar_nova_imagem(nova_img, img);
+
+    /* Gerar a imagem LBP */
+    /*
+     Exemplo:
+     ./lbp -i img1.tif -o img_out.tif
+     Nesse caso, o programa recebe como entrar imagem img1.tif e gerar a imagem LPB
+     com o nome informado pela opção -o. NENHUMA saída é esperada no terminal; caso
+     algum erro ocorra, basta não criar a imagem de saída.
+    */
+
+    /* Gera a imagem LBP */
+    gerar_lbp(img, nova_img);
+
+    // printf("gerando lbp:\n");
+
+    // for (int i = 0; i < nova_img->altura; i++)
+    // {
+    //     for (int j = 0; j < nova_img->largura; j++)
+    //     {
+    //         printf("%d ", nova_img->pixels[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+
+    /* Gera a imagem de saída */
+    gerar_imagem_saida(nova_img, arquivo_entrada);
+    // if (aux)
+    // {
+    //     FILE *arquivo_saida;
+    //     if (!(arquivo_saida = fopen(arquivo_saida, "wb"))) /* wb = write binary */
+    //     {
+    //         fprintf(stderr, "Erro ao abrir o arquivo de saída\n");
+    //         exit(EXIT_FAILURE);
+    //     }
+    //     gerar_imagem_saida(nova_img, arquivo_saida);
+    // }
+
+    /* Comparar uma imagem de teste com todas as imagens da base de referência */
+    /*  A saída deve ser EXATAMENTE a seguinte
+        Imagem mais similar: <img mais similar> <distância>
+    */
+
+    /* Exibir a imagem mais similar e a distância */
+    /* printf("Imagem mais similar: %s %.6f\n", img_similar, distancia); */
+
+    /*
+        em que <img mais similar> é a imagem com a menor distância Euclidiana encontrada
+        no diretório ./base (informar apenas o nome do arquivo com sua extensão) e
+        <distância> é o valor da distância associada a esta imagem, um número com
+        EXATAMENTE 6 casas decimais. Nome e valor devem ser separados APENAS por
+        um espaço.
+    */
+
+    // struct LBPHistograma *histograma = alocar_histograma();
+    // gerar_histograma(nova_img, histograma, arquivo_entrada);
+    fclose(arquivo); /* fecha o arquivo de entrada */
+
+    /* percorre o diretório */
+    // double distancia = 1e12;
+    // char menor_distancia[256];
+
+    /* ERRO A PARTIR DAQUI */
+    // ler_diretorio(diretorio);
+    //  encontrar_imagem_similar(diretorio, histograma, &distancia, menor_distancia);
+
+    // /* Libera a memória alocada */
+    // free (arquivo_entrada);
+    // free (arquivo_saida);
+    // free (diretorio);
+    // liberar_imagem(img);
+    // liberar_imagem(nova_img);
+    // free(histograma);
+
+    return 0;
+}
---
+++ trabalho1 (cópia)/lib.h
@@ -0,0 +1,111 @@
+/* Autora: Juliana Zambon (jz22), GRR20224168 */
+
+#ifndef LIB_H
+#define LIB_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Estruturas para armazenar dados da imagem e do histograma LBP */
+
+/* Estrutura para armazenar a imagem PGM */
+struct imagemPGM
+{
+    int largura;            /* Largura da imagem em pixels */
+    int altura;             /* Altura da imagem em pixels */
+    int maximo;             /* Valor máximo de cinza (branco) */
+    unsigned char **pixels; /* Matriz de pixels (valores de 0 a 255) */
+    char tipo[4];           /* Tipo da imagem (P2 ou P5) */
+};
+
+/* Estrutura para armazenar o histograma LBP */
+struct LBPHistograma
+{
+    int histograma[256]; /* Frequência de cada padrão LBP */
+    double tamanho;      /* Total de padrões LBP computados */
+};
+
+/*--------------------------------------------------------------------*/
+/* Funções para alocação de memória */
+
+/* Aloca memória para a imagem PGM */
+struct imagemPGM *alocar_imagem();
+
+/* Aloca memória para o histograma LBP */
+struct LBPHistograma *alocar_histograma();
+
+/* Aloca memória para os pixels da imagem */
+void alocar_pixels(struct imagemPGM *img);
+
+/*--------------------------------------------------------------------*/
+/* Funções para manipulação de pixels */
+
+/* Ignorar Linhas de Comentário: garantir que os comentários
+(linhas que começam com #) sejam ignorados ao ler arquivos P2 e P5*/
+void ignorar_comentarios(FILE *arquivo);
+
+/* Preenche a matriz de pixels para o formato P2 */
+struct imagemPGM *preencher_pixels_P2(FILE *arquivo, struct imagemPGM *img);
+
+/* Preenche a matriz de pixels para o formato P5 */
+struct imagemPGM *preencher_pixels_P5(FILE *arquivo, struct imagemPGM *img);
+
+/* Função para ler a imagem PGM */
+struct imagemPGM *ler_imagem(FILE *arquivo, struct imagemPGM *img, char *arquivo_entrada);
+
+/*--------------------------------------------------------------------*/
+/* Funções para processamento LBP */
+
+/* Inicializa uma nova imagem para armazenar o resultado do LBP */
+void inicializar_nova_imagem(struct imagemPGM *nova_img, struct imagemPGM *imagemPGM);
+
+/* Calcula o LBP para um pixel específico da imagem */
+/* Remover Bordas Durante a Convolução: garantir que as bordas da imagem original não sejam processadas.
+Assegurando que o cálculo comece após as bordas (por exemplo, começando de 1 até altura - 1 e largura - 1).*/
+void calcula_lbp(struct imagemPGM *img, struct imagemPGM *nova, int i, int j);
+
+/* Gera a imagem LBP a partir da imagem original */
+void gerar_lbp(struct imagemPGM *img, struct imagemPGM *nova);
+
+/*--------------------------------------------------------------------*/
+/* Funções de entrada e saída */
+
+/* Gera a imagem de saída no formato PGM */
+/* Formato de Saída: capaz de determinar o formato da imagem de entrada
+e gerar a saída correspondente no mesmo formato (P2 ou P5)*/
+void gerar_imagem_saida(struct imagemPGM *nova, const char *nome_arquivo_saida);
+
+/*--------------------------------------------------------------------*/
+/* Funções para histogramas */
+
+/* Gera o histograma LBP de uma imagem */
+/* Normalização do Histograma: garantindo que a contagem de pixels de cada tom seja dividida
+pelo total de pixels na imagem.*/
+void gerar_histograma(struct imagemPGM *nova, struct LBPHistograma *lbp_hist, char *arquivo_entrada);
+
+/* Calcula a distância euclidiana entre dois histogramas LBP */
+double distancia_euclidiana(struct LBPHistograma *hist1, struct LBPHistograma *hist2, struct LBPHistograma *comparacao);
+
+/* Lê um histograma LBP a partir de um arquivo de texto */
+struct LBPHistograma *ler_histLBP(FILE *arquivo, struct LBPHistograma *histograma);
+
+/* Exibir a imagem mais similar e a distância */
+/* printf("Imagem mais similar: %s %.6f\n", img_similar, distancia); */
+void encontrar_imagem_similar(char *diretorio, struct LBPHistograma *histograma_teste, double *distancia, char menor_distancia[256]);
+
+/*--------------------------------------------------------------------*/
+/* Funções de leitura de diretório */
+
+/* Lê o conteúdo de um diretório e retorna uma lista de arquivos de imagem */
+void ler_diretorio(char *nome_diretorio);
+
+/* Converte uma imagem PGM para o formato LBP */
+void converter_lbp(char arquivo_entrada[256]);
+
+/*--------------------------------------------------------------------*/
+/* Funções utilitárias */
+
+/* Libera a memória alocada para uma imagem PGM */
+void liberar_imagem(struct imagemPGM *img);
+
+#endif /* LIB_H */
